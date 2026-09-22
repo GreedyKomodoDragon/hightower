eb234e043a4828fcb8b9c23cc73a45967200f20a
Jake C <jakeychap@gmail.com>
2026-09-22 22:14:12 +0100

    enabling the tests

--- src/merkle/chunking_test.zig
+++ src/merkle/chunking_test.zig
@@ -21,62 +21,70 @@ test "MerkleizeChunks u8 produces one padded chunk" {
     );
 }
 
-// test "MerkleizeChunks u16 uses little endian and padding" {
-//     const chunks = ssz.MerkleizeChunks(@as(u16, 0x1234));
+test "MerkleizeChunks u16 uses little endian and padding" {
+    const allocator = std.testing.allocator;
+    const chunks = try merkle.Chunk(allocator, @as(u16, 0x1234));
+    defer allocator.free(chunks);
 
-//     try testing.expectEqual(@as(usize, 1), chunks.len);
+    try testing.expectEqual(@as(usize, 1), chunks.len);
 
-//     var expected = [_]u8{0} ** 32;
-//     expected[0] = 0x34;
-//     expected[1] = 0x12;
+    var expected = [_]u8{0} ** 32;
+    expected[0] = 0x34;
+    expected[1] = 0x12;
 
-//     try testing.expectEqualSlices(
-//         u8,
-//         &expected,
-//         &chunks[0],
-//     );
-// }
+    try testing.expectEqualSlices(
+        u8,
+        &expected,
+        &chunks[0],
+    );
+}
 
-// test "MerkleizeChunks u64 uses one chunk" {
-//     const chunks = ssz.MerkleizeChunks(@as(u64, 1));
+test "MerkleizeChunks u64 uses one chunk" {
+    const allocator = std.testing.allocator;
+    const chunks = try merkle.Chunk(allocator, @as(u64, 1));
+    defer allocator.free(chunks);
 
-//     try testing.expectEqual(@as(usize, 1), chunks.len);
+    try testing.expectEqual(@as(usize, 1), chunks.len);
 
-//     var expected = [_]u8{0} ** 32;
-//     expected[0] = 0x01;
+    var expected = [_]u8{0} ** 32;
+    expected[0] = 0x01;
 
-//     try testing.expectEqualSlices(
-//         u8,
-//         &expected,
-//         &chunks[0],
-//     );
-// }
+    try testing.expectEqualSlices(
+        u8,
+        &expected,
+        &chunks[0],
+    );
+}
 
-// test "MerkleizeChunks true produces 0x01 followed by zeroes" {
-//     const chunks = ssz.MerkleizeChunks(true);
+test "MerkleizeChunks true produces 0x01 followed by zeroes" {
+    const allocator = std.testing.allocator;
+    const chunks = try merkle.Chunk(allocator, true);
+    defer allocator.free(chunks);
 
-//     try testing.expectEqual(@as(usize, 1), chunks.len);
+    try testing.expectEqual(@as(usize, 1), chunks.len);
 
-//     var expected = [_]u8{0} ** 32;
-//     expected[0] = 0x01;
+    var expected = [_]u8{0} ** 32;
+    expected[0] = 0x01;
 
-//     try testing.expectEqualSlices(
-//         u8,
-//         &expected,
-//         &chunks[0],
-//     );
-// }
+    try testing.expectEqualSlices(
+        u8,
+        &expected,
+        &chunks[0],
+    );
+}
 
-// test "MerkleizeChunks false produces zero chunk" {
-//     const chunks = ssz.MerkleizeChunks(false);
+test "MerkleizeChunks false produces zero chunk" {
+    const allocator = std.testing.allocator;
+    const chunks = try merkle.Chunk(allocator, false);
+    defer allocator.free(chunks);
 
-//     try testing.expectEqual(@as(usize, 1), chunks.len);
+    try testing.expectEqual(@as(usize, 1), chunks.len);
 
-//     const expected = [_]u8{0} ** 32;
+    const expected = [_]u8{0} ** 32;
 
-//     try testing.expectEqualSlices(
-//         u8,
-//         &expected,
-//         &chunks[0],
-//     );
-// }
+    try testing.expectEqualSlices(
+        u8,
+        &expected,
+        &chunks[0],
+    );
+}
