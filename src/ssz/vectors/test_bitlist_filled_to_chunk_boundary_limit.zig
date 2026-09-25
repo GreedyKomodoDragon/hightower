996272ca549945636e0607a977fcacbb7aeb788e
Jake C <jakeychap@gmail.com>
2026-09-25 20:34:27 +0100

    fixed reference and added another test for bitlist overspill 43/118 now

--- src/ssz/vectors/test_bitlist_filled_to_chunk_boundary_limit.zig
+++ src/ssz/vectors/test_bitlist_filled_to_chunk_boundary_limit.zig
@@ -5,6 +5,66 @@
 // root: 0xbc16fae79b58a2e3dac0429d25b79cada399106276e08c5d3cfc3726db02b8ba
 // TODO: BoundaryBitList256 is not implemented in src/ssz/ssz.zig yet.
 
+const std = @import("std");
+const testing = std.testing;
+const ssz = @import("ssz");
+const types = @import("types");
+const merkle = @import("merkle");
+
 test "vectors.test_bitlist_filled_to_chunk_boundary_limit" {
-    return error.VectorNotImplemented;
+    const allocator = testing.allocator;
+
+    const BoundaryBitList256 = types.BitList(256);
+
+    const bits = [_]bool{true} ** 256;
+
+    const value = BoundaryBitList256{
+        .data = &bits,
+    };
+
+    var buf: [33]u8 = undefined;
+    var writer: std.Io.Writer = .fixed(&buf);
+
+    try ssz.serialize(
+        &writer,
+        value,
+    );
+
+    try testing.expectEqualSlices(
+        u8,
+        &.{
+            0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff,
+            0x01,
+        },
+        writer.buffered(),
+    );
+
+    const root = try merkle.HashTreeRoot(
+        allocator,
+        value,
+    );
+
+    const expected_root = [_]u8{
+        0xbc, 0x16, 0xfa, 0xe7,
+        0x9b, 0x58, 0xa2, 0xe3,
+        0xda, 0xc0, 0x42, 0x9d,
+        0x25, 0xb7, 0x9c, 0xad,
+        0xa3, 0x99, 0x10, 0x62,
+        0x76, 0xe0, 0x8c, 0x5d,
+        0x3c, 0xfc, 0x37, 0x26,
+        0xdb, 0x02, 0xb8, 0xba,
+    };
+
+    try testing.expectEqualSlices(
+        u8,
+        &expected_root,
+        &root,
+    );
 }
